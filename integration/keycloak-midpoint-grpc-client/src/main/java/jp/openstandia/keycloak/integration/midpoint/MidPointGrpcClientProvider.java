43748759e5bf5c7ef6ab2f4925c2824350fd3e41
Hiroyuki Wada <h2-wada@nri.co.jp>
2023-11-24 10:59:37 +0900

    feat: enhanced API for SelfServiceResource stub acquisition
    (cherry picked from commit 923c93222bb3a85f6c33281bc950120818fdbd8a)

--- integration/keycloak-midpoint-grpc-client/src/main/java/jp/openstandia/keycloak/integration/midpoint/MidPointGrpcClientProvider.java
+++ integration/keycloak-midpoint-grpc-client/src/main/java/jp/openstandia/keycloak/integration/midpoint/MidPointGrpcClientProvider.java
@@ -1,14 +1,22 @@
 package jp.openstandia.keycloak.integration.midpoint;
 
+import io.grpc.Metadata;
 import io.grpc.StatusRuntimeException;
 import jp.openstandia.midpoint.grpc.SelfServiceResourceGrpc;
 import org.keycloak.provider.Provider;
 
 import java.util.Optional;
+import java.util.function.Consumer;
 
 public interface MidPointGrpcClientProvider extends Provider {
 
+    SelfServiceResourceGrpc.SelfServiceResourceBlockingStub getSelfServiceResource();
+
     SelfServiceResourceGrpc.SelfServiceResourceBlockingStub getSelfServiceResource(String username);
+
+    SelfServiceResourceGrpc.SelfServiceResourceBlockingStub getSelfServiceResourceByOid(String oid);
+
+    SelfServiceResourceGrpc.SelfServiceResourceBlockingStub getSelfServiceResourceWithMetadataCustomizer(Consumer<Metadata> metadataCustomizer);
 
     Optional<MidPointPolicyFormMessage> handlePolicyError(StatusRuntimeException e);
 
